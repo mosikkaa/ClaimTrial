27e48382bdc19ec3b88e2ebf5a189ab94dbabf55
Luka Meskhi <meskhiluk@gmail.com>
2025-12-14 17:50:57 +0400

    create app

---
+++ claim-trial/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Section from "./components/Section/Section.tsx";
+
+function App() {
+  return (
+    <>
+        <Section/>
+    </>
+  )
+}
+
+export default App
---
+++ claim-trial/src/components/Section/Section.tsx
@@ -0,0 +1,13 @@
+
+
+const Section = () => {
+    return (
+        <section>
+            <div>
+                <h1 className='font-poppins font-bold text-white text-[50px] leading-[55px] tracking-[-0.52px]'>Learn to code by watching others</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Section
